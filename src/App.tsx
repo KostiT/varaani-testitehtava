b75ab3c8e01a998e6754305f67bacda83e12a1a4
Kosti Tsinovski - freelancer <kosti.tsinovski@phz.fi>
2021-05-28 17:39:18 +0300

    Deleting comments

--- src/App.tsx
+++ src/App.tsx
@@ -19,31 +19,13 @@ const App: React.FC = () => {
 	}, [state]);
   return !isLoaded ? null : (
     <div className="App">
-      {/* <GlobalStateContext.Provider value={{ state, dispatch }}> */}
         <Router>
-          {/* {
-            state.isLogin && 
-              <div>
-                <Navbar className = 'topnav' collapseOnSelect expand="md">
-                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                  <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                      <Nav.Link href="/home">Home</Nav.Link>
-                      <Nav.Link href="/next">Next</Nav.Link>
-                      <Nav.Link href="http://google.com/">Google</Nav.Link>
-                    </Nav>
-                  </Navbar.Collapse>
-                </Navbar>
-               
-              </div>
-          } */}
           <Switch>
             <Route exact path="/home" component={HomePage}></Route>
             <Route path="/next" component={Test}></Route>
             <Route path="/" component={LoginPage}></Route>
           </Switch>
         </Router>
-      {/* </GlobalStateContext.Provider> */}
     </div>
   );
 };
